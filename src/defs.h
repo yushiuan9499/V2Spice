5030b7bb1fb6f32915c56b79c087c2ac7d010d90
yushiuan9499 <yushiuan9499@gmail.com>
2026-05-09 20:57:13 +0800

    feat: support binary operator
    To support for-loop and some basic parameter & constant operation, we
    need to support binary operators, such as multiplication and addition.
    
    Here I RL to parse binary operator and evaluate the value at sema phase.

--- src/defs.h
+++ src/defs.h
@@ -11,18 +11,37 @@ enum TokenType {
     TOEKN_TYPE_LBRACKET,   // [
     TOKEN_TYPE_RBRACKET,   // ]
     TOKEN_TYPE_SEMICOLON,  // ;
-    TOKEN_TYPE_EQUAL,      // =
     TOKEN_TYPE_HASHTAG,    // #
+    TOKEN_TYPE_DOT,        // .
+    /* Following are binary operators. */
     TOKEN_TYPE_COMMA,      // ,
-    TOKEN_TYPE_DOT,        // ,
-    TOKEN_TYPE_PUNCT_END = TOKEN_TYPE_DOT,
+    TOKEN_TYPE_EQUAL,      // =
+    TOKEN_TYPE_EQUAL2,     // ==
+    TOKEN_TYPE_NOT_EQUAL,  // !=
+    TOKEN_TYPE_LT,         // <
+    TOKEN_TYPE_GT,         // >
+    TOKEN_TYPE_LE,         // <=
+    TOKEN_TYPE_GE,         // >=
+    TOKEN_TYPE_NOT,        // !
+    TOKEN_TYPE_AND,        // &&
+    TOKEN_TYPE_OR,         // ||
+    TOKEN_TYPE_QUESTION,   // ?
+    TOKEN_TYPE_COLON,      // :
+    TOKEN_TYPE_PLUS,       // +
+    TOKEN_TYPE_MINUS,      // -
+    TOKEN_TYPE_ASTERISK,   // *
+    TOKEN_TYPE_SLASH,      // /
+    TOKEN_TYPE_PERCENT,    // %
+    TOKEN_TYPE_PUNCT_END = TOKEN_TYPE_PERCENT,
 
     TOKEN_TYPE_ASSIGN,     // assign
     TOKEN_TYPE_MODULE,     // module
     TOKEN_TYPE_ENDMODULE,  // endmodule
     TOKEN_TYPE_WIRE,       // wire
     TOKEN_TYPE_PARAM,      // parameter
-    TOKEN_TYPE_KEYWORD_END = TOKEN_TYPE_PARAM,
+    TOKEN_TYPE_FOR,        // for
+    TOKEN_TYPE_GENVAR,     // genvar
+    TOKEN_TYPE_KEYWORD_END = TOKEN_TYPE_GENVAR,
 
     TOKEN_TYPE_NUMBER,      // number literal
     TOKEN_TYPE_IDENTIFIER,  // identifier
@@ -30,15 +49,42 @@ enum TokenType {
 };
 
 static const char *token_str[TOKEN_TYPE_NR] = {
-    [TOKEN_TYPE_NONE] = "",           [TOKEN_TYPE_EOF] = "<EOF>",
-    [TOKEN_TYPE_LPAREN] = "(",        [TOKEN_TYPE_RPAREN] = ")",
-    [TOEKN_TYPE_LBRACKET] = "[",      [TOKEN_TYPE_RBRACKET] = "]",
-    [TOKEN_TYPE_SEMICOLON] = ";",     [TOKEN_TYPE_EQUAL] = "=",
-    [TOKEN_TYPE_HASHTAG] = "#",       [TOKEN_TYPE_COMMA] = ",",
-    [TOKEN_TYPE_DOT] = ".",           [TOKEN_TYPE_ASSIGN] = "assign",
-    [TOKEN_TYPE_MODULE] = "module",   [TOKEN_TYPE_ENDMODULE] = "endmodule",
-    [TOKEN_TYPE_WIRE] = "wire",       [TOKEN_TYPE_PARAM] = "parameter",
-    [TOKEN_TYPE_NUMBER] = "<number>", [TOKEN_TYPE_IDENTIFIER] = "<identifier>"};
+    [TOKEN_TYPE_NONE] = "",
+    [TOKEN_TYPE_EOF] = "<EOF>",
+    [TOKEN_TYPE_LPAREN] = "(",
+    [TOKEN_TYPE_RPAREN] = ")",
+    [TOEKN_TYPE_LBRACKET] = "[",
+    [TOKEN_TYPE_RBRACKET] = "]",
+    [TOKEN_TYPE_SEMICOLON] = ";",
+    [TOKEN_TYPE_HASHTAG] = "#",
+    [TOKEN_TYPE_DOT] = ".",
+    [TOKEN_TYPE_COMMA] = ",",
+    [TOKEN_TYPE_EQUAL] = "=",
+    [TOKEN_TYPE_EQUAL2] = "==",
+    [TOKEN_TYPE_NOT_EQUAL] = "!=",
+    [TOKEN_TYPE_LT] = "<",
+    [TOKEN_TYPE_GT] = ">",
+    [TOKEN_TYPE_LE] = "<=",
+    [TOKEN_TYPE_GE] = ">=",
+    [TOKEN_TYPE_NOT] = "!",
+    [TOKEN_TYPE_AND] = "&&",
+    [TOKEN_TYPE_OR] = "||",
+    [TOKEN_TYPE_QUESTION] = "?",
+    [TOKEN_TYPE_COLON] = ":",
+    [TOKEN_TYPE_PLUS] = "+",
+    [TOKEN_TYPE_MINUS] = "-",
+    [TOKEN_TYPE_ASTERISK] = "*",
+    [TOKEN_TYPE_SLASH] = "/",
+    [TOKEN_TYPE_PERCENT] = "%",
+    [TOKEN_TYPE_ASSIGN] = "assign",
+    [TOKEN_TYPE_MODULE] = "module",
+    [TOKEN_TYPE_ENDMODULE] = "endmodule",
+    [TOKEN_TYPE_WIRE] = "wire",
+    [TOKEN_TYPE_PARAM] = "parameter",
+    [TOKEN_TYPE_FOR] = "for",
+    [TOKEN_TYPE_GENVAR] = "genvar",
+    [TOKEN_TYPE_NUMBER] = "<number>",
+    [TOKEN_TYPE_IDENTIFIER] = "<identifier>"};
 
 struct Idx {
     unsigned start;
@@ -55,8 +101,9 @@ enum AstType {
     AST_TYPE_MODULE_DECL,
     AST_TYPE_MODULE_INST,
     AST_TYPE_WIRE_DECL,
-    AST_TYPE_ASSIGN,
-    AST_TYPE_PARAM_ASSIGN,
+    AST_TYPE_BINARY_OP,
+    AST_TYPE_ID,
+    AST_TYPE_NUMBER,
     AST_TYPE_NR
 };
 struct Ast {
@@ -65,26 +112,41 @@ struct Ast {
     virtual ~Ast() = default;
 };
 
+struct IdAst : public Ast {
+    Idx name;
+    IdAst() : Ast{AST_TYPE_ID} {}
+};
+
+struct NumberAst : public Ast {
+    bool is_float;  // genvar is not float. other number literals are float.
+    union {
+        long long int_value;
+        double float_value;
+        Idx pos;
+    };
+    NumberAst() : Ast{AST_TYPE_NUMBER} {}
+};
+
+struct BinaryOpAst : public Ast {
+    Token op;
+    Ast *lhs;
+    Ast *rhs;
+    BinaryOpAst() : Ast{AST_TYPE_BINARY_OP}, lhs(nullptr), rhs(nullptr) {}
+    ~BinaryOpAst()
+    {
+        delete lhs;
+        delete rhs;
+    }
+};
+
 struct WireDeclAst : public Ast {
     Idx name;
     WireDeclAst() : Ast{AST_TYPE_WIRE_DECL} {}
 };
 
-struct AssignAst : public Ast {
-    Idx lhs;
-    Idx rhs;
-    AssignAst() : Ast{AST_TYPE_ASSIGN} {}
-};
-
-struct ParamAssignAst : public Ast {
-    Idx param_name;
-    Idx value;
-    ParamAssignAst() : Ast{AST_TYPE_PARAM_ASSIGN} {}
-};
-
 struct ModuleDeclAst : public Ast {
     Idx name;
-    std::vector<ParamAssignAst *> params;
+    std::vector<BinaryOpAst *> params;
     std::vector<Idx> ports;
     std::vector<Ast *> body;
     ModuleDeclAst() : Ast{AST_TYPE_MODULE_DECL} {}
@@ -103,7 +165,7 @@ struct ModuleInstAst : public Ast {
     Idx module_name;
     Idx instance_name;
     bool use_named_port;
-    std::vector<ParamAssignAst *> params;
+    std::vector<BinaryOpAst *> params;
     union Port {
         Idx positional;
         struct {
