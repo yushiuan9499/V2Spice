232bf342f95d696df9d482386f14156bc87a0f09
yushiuan9499 <yushiuan9499@gmail.com>
2026-05-13 14:32:55 +0800

    test: add test cases

--- tests/test.v
+++ tests/test.v
@@ -33,7 +33,13 @@ n_18 M2 #(W=0.5u,L=18u)(
 );
 endmodule
 
-module no_port();
+module no_port_and_use_array();
+n_18 M1 #(W=1u*2,L=18u/2u*3u+1u)(
+    .D(a[0]),
+    .G(a[1]),
+    .S(a[2]),
+    .B(a[3])
+);
 endmodule
 
 module lots_of_binary_op();
@@ -44,3 +50,5 @@ n_18 M1 #(W=1u*2,L=18u/2u*3u+1u)(
     .B(d)
 );
 endmodule
+
+$spice("V1 VDD 0 DC 1.8V");
